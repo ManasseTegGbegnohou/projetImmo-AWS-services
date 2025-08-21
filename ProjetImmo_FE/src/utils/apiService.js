f5216b2c5b6db557387631ea8d0eb6cc288889f0
“ManasseTegGbegnohou” <mtgbegnohou@gmail.com>
2025-08-20 20:02:00 -0400

    Update VITE_API_URL in docker-compose and apiService.js

--- ProjetImmo_FE/src/utils/apiService.js
+++ ProjetImmo_FE/src/utils/apiService.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-
-const API_URL = import.meta.env.VITE_API_URL || ""
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8888/api";
 
 export const getAllHouses = async () => {
     try {
